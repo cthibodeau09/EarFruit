980120f864d9d35c27b73e3ffbeda6cd513b8c23
Jay Thornton <jaythornton@Jays-MacBook-Air.local>
2023-05-13 20:01:16 -0400

    added to server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+//imports
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const roots = require('./controllers');
+
+//create the connection to the DB
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//create the express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//set up sessions with cookies
+const sess = {
+    secret: "secret",
+    cookie: {
+
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
