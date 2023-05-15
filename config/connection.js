c4798298424dc503520809d77ebf1c67d98492c2
KnightHawk1090 <121210332+KnightHawk1090@users.noreply.github.com>
2023-05-15 17:28:50 -0500

    updated connect.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,29 @@
-const Sequelize = require('sequelize');
+const axios = require('axios');
 require('dotenv').config();
 
-let sequelize;
+const discogsAccessToken = process.env.DISCOGS_ACCESS_TOKEN;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-  } else {
-    sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306,
-        name: randomName
-      }
-    );
+// Example function to search for songs, albums, or artists using the Discogs API
+async function search(query, type) {
+  try {
+    const response = await axios.get('https://api.discogs.com/database/search', {
+      params: {
+        q: query,
+        // before demonstrating or testing, replaced with the current access token
+        token: discogsAccessToken,
+        type: type,
+      },
+    });
+
+    const results = response.data.results;
+    return results;
+  } catch (error) {
+    console.error('Error searching:', error);
+    throw error;
   }
-  
-  module.exports = sequelize;
+}
+
+module.exports = {
+  search,
+};
+
