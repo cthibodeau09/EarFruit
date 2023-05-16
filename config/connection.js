4e07e14aa62a773b44743205b568de79e8481f4e
Jay Thornton <jaythornton@Jays-MacBook-Air.local>
2023-05-15 20:24:11 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,30 @@
-const axios = require('axios');
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const discogsAccessToken = process.env.DISCOGS_ACCESS_TOKEN;
+let sequelize;
 
-// Example function to search for songs, albums, or artists using the Discogs API
-async function search(query, type) {
-  try {
-    const response = await axios.get('https://api.discogs.com/database/search', {
-      params: {
-        q: query,
-        // before demonstrating or testing, replaced with the current access token
-        token: discogsAccessToken,
-        type: type,
-      },
-    });
-
-    const results = response.data.results;
-    return results;
-  } catch (error) {
-    console.error('Error searching:', error);
-    throw error;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
+    );
   }
+<<<<<<< HEAD
 }
 
 module.exports = {
   search,
 };
 
+=======
+  
+  module.exports = sequelize;
+>>>>>>> d57730e (TA_test)
