7fe41ab6f670786340fd92adfd9b44204f4f0bed
jarvis <jthorn1017@gmail.com>
2023-05-17 18:55:29 -0400

    testing

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const axios = require('axios');
 require('dotenv').config();
 
@@ -22,3 +23,33 @@ async function search(query, type) {
     throw error;
   }
 }
+<<<<<<< HEAD
+=======
+
+module.exports = {
+  search,
+};
+=======
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
+    );
+  }
+  
+  module.exports = sequelize;
+>>>>>>> 6229143 (connection.js and .env base)
+>>>>>>> 4a053aa (connection.js and .env base)
